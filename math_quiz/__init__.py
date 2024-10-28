431b427cec5b0a85c62422f56ad007e11f462785
Vibhugupta10616 <vibhu7gupta@gmail.com>
2024-10-28 22:27:29 +0100

    it is now an installable package

---
+++ math_quiz/__init__.py
@@ -0,0 +1,6 @@
+from . import quiz
+
+# __init__.py
+
+# This file marks the directory as a Python package and can be used to initialize package-level variables or import submodules.
+# The __init__.py file imports the quiz module from the current package. This allows the quiz module to be accessed directly from the package without specifying the full path.
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsss_homework_2',
+    version='0.1',
+    packages=find_packages(),
+
+    author='Vibhu Gupta',
+    description='A github repository for the second homework of the DSSS course.',
+    url='https://github.com/yourusername/dsss_homework_2',  # Update with your project's URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
